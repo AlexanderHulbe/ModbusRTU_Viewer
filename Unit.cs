b7f9359fd4c30c51d279c23024af281b9c45b959
Programming-Beast <alexander@hulbe.de>
2023-03-02 13:53:40 +0100

    added Comments to Unit.cs

--- Unit.cs
+++ Unit.cs
@@ -4,8 +4,10 @@ namespace ModbusRTU_Viewer
 {
     public class Unit
     {
+        // Declare Variable
         public String Name { get; set; }
 
+        // Default Constructor for Unit
         public Unit(String Unit)
         {
             this.Name = Unit;
